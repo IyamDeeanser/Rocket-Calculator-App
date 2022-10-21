26d0c4fe0fd6fcfa7bf8f414eec2c3c97586348a
IyamDeeanser <TimothyCai2004@gmail.com>
2022-10-21 14:59:51 -0700

    added steps 8 - 13

--- calculations.js
+++ calculations.js
@@ -13,23 +13,44 @@ export class Calculations {
         return Math.sqrt((((1 + y) / 2) ** ((y + 1) / (y - 1))) / y * GAS_CONSTANT * (Tchamber / Mmolprop));
     }
 
-    /*
-    * y: gamma
-    * M: mach
-    * returns throat to exit area ratio
-    */
     areaRatio = (y, M) => {
         return ((((y - 1)/2 * M**2 + 1)*2/(y + 1))**((y + 1)/((y - 1) * 2))) / M;
     }
 
-    /*
-    */
-   p_inj = (p_cns, y, M_i) => {
-        return (p_cns*(1 + y * M_i**2)/((1+(y-1)/(2)*M_i**2))**(y/(y-1)));
-   }
+    p_inj = (p_cns, y, M_i) => {
+            return (p_cns * (1 + y * M_i**2)/((1 + (y-1)/(2)*M_i**2))**(y/(y - 1)));
+    }
 
+    p_t = (p_cns, y) => {
+        return (p_cns * ((2/(y + 1))**(y/(y - 1))));
+    }
 
-   p_t = (p_cns, y) => {
-    return (p_cns*(2/(y+1)**(y/(y-1))));
-}
+    p_e = (p_cns, y, M_e) => {
+        return (p_cns*((1 + (y-1)/2 * M_e**2)**(y/(y-1)))**-1);
+    }
+
+    A_t = (M, c_Star, p_cns) => {
+        return (M * (c_Star/p_cns));
+    }
+
+    io_Calc = (area, ratio) => {
+        return (ratio * area);
+    }
+
+    areaToDiameter = (area) => {
+        return (2 * Math.sqrt(area/Math.PI));
+    }
+
+    nozzle_L = (diameter, throat, half_Angle) => {
+        return ((diameter - throat)/(2*Math.tan(half_Angle)));
+    }
+
+    volume = (radius, radius_Throat, length) => {
+        return ((Math.PI * length * (radius*radius + radius*radius_Throat + radius_Throat*radius_Throat))/3);
+    }
+
+    L_c = (area_Throat, L_STAR, area_chamber, V_con) => {
+        return ((area_Throat*L_STAR - V_con)/area_chamber);
+    }
+    
 }
--- main.js
+++ main.js
@@ -5,20 +5,58 @@ import { print } from "./print.js";
 const NOZZLE_STAGNATION_PRESSURE = 750000; // Pa
 const FLOW_RATE = 0.04; // kg/s
 const EXPANSION_RATIO = 2.4; // 2.4
+const EXPANSION_HALF_ANGLE = Math.PI/12;
+const CONTRACTION_RATIO = 3;
+const CONTRACTION_HALF_ANGLE = Math.PI/6;
 const SELECT_PROPELLANTS = "Kerosene-GOX";
 const OXIDIZER_FUEL_RATIO = 2.1;
+const L_STAR = 1.1;
+
 
 // Results from CEARUN
 const FLAME_TEMP = 3256.57 // K
 const MOLECULAR_WEIGHT = 20.788 // kg/kmol
 const GAMMA = 1.1407 
 const IDEAL_C_STAR = 1792.1 // m/s
+const C_STAR_EFFICIENCY = 1.00
+const C_STAR = IDEAL_C_STAR*C_STAR_EFFICIENCY;
+
+//calculation variables
+let M_i = 0.257;    //mach number at inlet   
+let M_e = 2.163;    //mach number at exit
+let A_t = 0;        //area of throat cross-section
+let A_c = 0;        //area of chamber cross-section
+let A_e = 0;        //area of exit cross-section
+let D_t = 0;        //diameter of throat
+let D_c = 0;        //diameter of chamber
+let D_e = 0;        //diameter of exit
+let n_L_C = 0;      //nozzle converging length
+let V_con = 0;      //volume of truncated converging cone
 
 let calc = new Calculations();
 
 // print calculations
+A_t = calc.A_t(FLOW_RATE, C_STAR, NOZZLE_STAGNATION_PRESSURE); //issue with g vs M
+D_t = calc.areaToDiameter(A_t);
+A_c = calc.io_Calc(A_t, CONTRACTION_RATIO);
+D_c = calc.areaToDiameter(A_c);
+A_e = calc.io_Calc(A_t, EXPANSION_RATIO);
+D_e = calc.areaToDiameter(A_e);
+n_L_C = calc.nozzle_L(D_c, D_t, CONTRACTION_HALF_ANGLE);
+V_con = calc.volume(D_c/2, D_t/2, n_L_C)
 
-print(calc.cStar(GAMMA, FLAME_TEMP, MOLECULAR_WEIGHT));
-print(calc.areaRatio(GAMMA, FLOW_RATE));
-print(calc.p_inj(NOZZLE_STAGNATION_PRESSURE, GAMMA, 0.257));
-print(calc.p_t(NOZZLE_STAGNATION_PRESSURE, GAMMA));
+print("C*   -   " + calc.cStar(GAMMA, FLAME_TEMP, MOLECULAR_WEIGHT));
+print("Area Ratio   -   " + calc.areaRatio(GAMMA, FLOW_RATE));
+print("Injector Pressure   -   " + calc.p_inj(NOZZLE_STAGNATION_PRESSURE, GAMMA, M_i));
+print("Throat Pressure   -   " + calc.p_t(NOZZLE_STAGNATION_PRESSURE, GAMMA));
+print("Exit Pressure   -   " + calc.p_e(NOZZLE_STAGNATION_PRESSURE, GAMMA, M_e));
+print("Throat Area   -   " + A_t);
+print("Throat Diameter   -   " + D_t);
+print("Chamber Area   -   " + A_c);
+print("Chamber Diameter   -   " + D_c);
+print("Exit Area   -   " + A_e);
+print("Exit Diameter   -   " + D_e);
+print("Contraction Length   -   " + calc.nozzle_L(D_c, D_t, CONTRACTION_HALF_ANGLE));
+print("Expansion Length   -   " + calc.nozzle_L(D_e, D_t, EXPANSION_HALF_ANGLE));
+print("Contraction Volume   -   " + V_con);
+print("L* Length   -   " + calc.L_c(A_t, L_STAR, A_c, V_con));
